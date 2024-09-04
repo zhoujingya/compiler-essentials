cabcaa72fbf155f08d58c211945322824a9a6799
zhoujingya <jing.zhou@terapines.com>
2024-09-04 22:06:59 +0800

    Init loop tutorial

---
+++ loop/LoopOptTutorial.cpp
@@ -0,0 +1,47 @@
+//===-------- LoopOptTutorial.cpp - Loop Opt Tutorial Pass ------*- C++ -*-===//
+//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+/// This file contains a small loop pass to be used to illustrate several
+/// aspects about writing a loop optimization. It was developed as part of the
+/// "Writing a Loop Optimization" tutorial, presented at LLVM Devepeloper's
+/// Conference, 2019.
+//===----------------------------------------------------------------------===
+
+#include "LoopOptTutorial.h"
+#include "llvm/Analysis/LoopPass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "loop-opt-tutorial"
+
+bool LoopSplit::run(Loop &L) const {
+
+  LLVM_DEBUG(dbgs() << "Entering " << __func__ << "\n");
+
+  LLVM_DEBUG(dbgs() << "TODO: Need to check if Loop is a valid candidate\n");
+
+  return false;
+}
+
+PreservedAnalyses LoopOptTutorialPass::run(Loop &L, LoopAnalysisManager &LAM,
+                                           LoopStandardAnalysisResults &AR,
+                                           LPMUpdater &U) {
+  LLVM_DEBUG(dbgs() << "Entering LoopOptTutorialPass::run\n");
+  LLVM_DEBUG(dbgs() << "Loop: "; L.dump(); dbgs() << "\n");
+
+  bool Changed = LoopSplit(AR.LI).run(L);
+
+  if (!Changed)
+    return PreservedAnalyses::all();
+
+  return llvm::getLoopPassPreservedAnalyses();
+}
---
+++ loop/LoopOptTutorial.h
@@ -0,0 +1,46 @@
+//===-------- LoopOptTutorial.h - Loop Opt Tutorial Pass ------*- C++ -*-===//
+//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+/// \file
+/// This file contains a small loop pass to be used to illustrate several
+/// aspects about writing a loop optimization. It was developed as part of the
+/// "Writing a Loop Optimization" tutorial, presented at LLVM Devepeloper's
+/// Conference, 2019.
+//===----------------------------------------------------------------------===
+
+#ifndef LLVM_TRANSFORMS_SCALAR_LOOPOPTTUTORIAL_H
+#define LLVM_TRANSFORMS_SCALAR_LOOPOPTTUTORIAL_H
+
+#include "llvm/Analysis/LoopAnalysisManager.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Transforms/Utils/ValueMapper.h"
+
+namespace llvm {
+
+class Loop;
+class LPMUpdater;
+
+class LoopSplit {
+public:
+  LoopSplit(LoopInfo &LI) : LI(LI) {}
+
+  bool run(Loop &L) const;
+
+private:
+  LoopInfo &LI;
+};
+
+class LoopOptTutorialPass : public PassInfoMixin<LoopOptTutorialPass> {
+public:
+  PreservedAnalyses run(Loop &L, LoopAnalysisManager &AM,
+                        LoopStandardAnalysisResults &AR, LPMUpdater &U);
+};
+
+} // end namespace llvm
+
+#endif // LLVM_TRANSFORMS_SCALAR_LOOPOPTTUTORIAL_H
