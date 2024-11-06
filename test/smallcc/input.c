2b618fa532a6d8d67d33f8a2027f1f13a0f80be9
zhoujingya <jing.zhou@terapines.com>
2024-11-06 22:21:43 +0800

    Fix small cc test file

--- test/smallcc/input.c
+++ test/smallcc/input.c
@@ -1,3 +1,4 @@
 // RUN: smallcc -input-string="123" > /tmp/test.s
 // RUN: riscv64-unknown-linux-gnu-gcc /tmp/test.s  -static -o /tmp/a.out
 // RUN: python3 %utils_path/qemu-test.py /tmp/a.out 123
+// REQUIRES: smallcc
